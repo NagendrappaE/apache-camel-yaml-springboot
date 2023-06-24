e7807aa7398312b63767c7a0cadded0de354cb8a
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-06-24 21:08:34 +0530

    gson changes

--- src/main/java/com/ece/camel/yaml/spring/controller/StudentController.java
+++ src/main/java/com/ece/camel/yaml/spring/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.ece.camel.yaml.spring.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,23 +10,57 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ece.camel.yaml.spring.bean.Datum;
+import com.ece.camel.yaml.spring.bean.HopexResponse;
+import com.ece.camel.yaml.spring.bean.OrganizationalProcessOwnerOrganizationalProcess;
 import com.ece.camel.yaml.spring.bean.StudentResp;
-
 
 @RequestMapping
 @RestController
 public class StudentController {
 
 	@PostMapping("savestudent")
-	public StudentResp getStudent(HttpServletRequest request,HttpServletResponse response) {
-		
-		System.out.println("the IP address "+request.getRemoteAddr());
+	public StudentResp getStudent(HttpServletRequest request, HttpServletResponse response) {
+
+		System.out.println("the IP address " + request.getRemoteAddr());
 
 		StudentResp resp = new StudentResp();
 
 		resp.setSage(String.valueOf(LocalDate.now()));
 		resp.setSalary("500000");
 		resp.setSname("NAGENDRA");
+		return resp;
+
+	}
+
+	@PostMapping("getapex")
+	public HopexResponse getApexRespo(HttpServletRequest request, HttpServletResponse response) {
+
+		System.out.println("the IP address " + request.getRemoteAddr());
+
+		ArrayList<Datum> dataList = new ArrayList<>();
+		Datum daa = new Datum();
+
+		ArrayList<OrganizationalProcessOwnerOrganizationalProcess> organizationalProcess_OwnerOrganizationalProces = new ArrayList<>();
+
+		OrganizationalProcessOwnerOrganizationalProcess proce = new OrganizationalProcessOwnerOrganizationalProcess();
+
+		proce.setId("XXXX{][]ss");
+		proce.setName("udud907082072eef33'..']p/./../.");
+
+		organizationalProcess_OwnerOrganizationalProces.add(proce);
+
+		daa.setId("Sibhhhhh");
+		daa.setName("[Duplicated - CHARVE] Organization processs");
+		daa.setOrganizationalProcess_OwnerOrganizationalProcess(organizationalProcess_OwnerOrganizationalProces);
+
+		for (int i = 0; i < 2; i++) {
+			dataList.add(daa);
+		}
+
+		HopexResponse resp = new HopexResponse();
+		resp.setData(dataList);
+
 		return resp;
 
 	}
--- src/main/java/com/ece/camel/yaml/spring/processor/JsonToMapProcessor.java
+++ src/main/java/com/ece/camel/yaml/spring/processor/JsonToMapProcessor.java
@@ -1,5 +1,6 @@
 package com.ece.camel.yaml.spring.processor;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
@@ -7,11 +8,8 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.Criteria;
-import com.jayway.jsonpath.Filter;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.Option;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Component
 public class JsonToMapProcessor implements Processor {
@@ -20,18 +18,30 @@ public class JsonToMapProcessor implements Processor {
 	public void process(Exchange exchange) throws Exception {
 
 		String jsonDataSourceString = exchange.getIn().getBody(String.class);
-		System.out.println(jsonDataSourceString);
 
-		Configuration configuration = Configuration.builder().options(Option.DEFAULT_PATH_LEAF_TO_NULL).build();
+		
+		Gson gson = new Gson();
 
+		Type mapType = new TypeToken<Map<String, Object>>() {
+		}.getType();
+		Map<String, Object> map = gson.fromJson(jsonDataSourceString, mapType);
 
-		Filter expensiveFilter = Filter.filter(Criteria.where("id").notEmpty());
-		List<Map<String, Object>> expensive = JsonPath.parse(jsonDataSourceString,configuration).read("$['data'][?]",
-				expensiveFilter);
+		List<Map<String, Object>> dataList = (List<Map<String, Object>>) map.get("data");
 
-		System.out.println(expensive);
+		/*
+		 * Configuration configuration =
+		 * Configuration.builder().options(Option.SUPPRESS_EXCEPTIONS).build();
+		 */
+		
+		/*
+		 * Filter expensiveFilter = Filter.filter(Criteria.where("id").notEmpty());
+		 * List<Map<String, Object>> expensive =
+		 * JsonPath.parse(s2,configuration).read("$['data']", expensiveFilter);
+		 */
 
-		exchange.getIn().setBody(expensive);
+		System.out.println(dataList);
+
+		exchange.getIn().setBody(dataList);
 
 	}
 
