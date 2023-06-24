7d3db215bf185692f36a9035ca378da4671f7ddb
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-06-24 11:36:20 +0530

    apache camel initial setup

---
+++ src/test/java/com/ece/camel/yaml/spring/ApacheCamelYamlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.camel.yaml.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelYamlSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
