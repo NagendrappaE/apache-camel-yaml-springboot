3cf7209c23f15f1b91aeafef69535531f8c73131
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-08-23 18:12:36 +0530

    junit test case

--- src/test/java/com/ece/camel/yaml/spring/HeaderConfigurerTest.java
+++ src/test/java/com/ece/camel/yaml/spring/HeaderConfigurerTest.java
@@ -2,14 +2,16 @@ package com.ece.camel.yaml.spring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.ece.camel.yaml.spring.configurer.HeaderConfigurer;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,24 +19,28 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HeaderConfigurerTest {
-	
-	private HeaderConfigurer headerConfigurer;
-	
-	@BeforeEach
-    public void setUp() {
-		headerConfigurer = new HeaderConfigurer(); 
-    }
+    
+    @Mock
+    private ObjectMapper objectMapper;
 
-	@Test
+    @InjectMocks
+    private HeaderConfigurer headerConfigurer;
+
+    public HeaderConfigurerTest() {
+        MockitoAnnotations.openMocks(this);
+    }
+    
+    @Test
     public void testGenericMapData() throws JsonMappingException, JsonProcessingException {
         // Create a sample data map
-        
-        String jsonStr="{\"ETL_ID\":\"John\",\"ETL_TS\":\"12-02-2023\",\"CONTENT\":{\"name\":\"abc\"}}";
+        String jsonStr = "{\"ETL_ID\":\"John\",\"ETL_TS\":\"12-02-2023\",\"CONTENT\":{\"name\":\"abc\"}}";
         Map<String, Object> data = new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
-
 
         // Specify the jsonStringKey
         String jsonStringKey = "CONTENT";
+
+        // Mock the behavior of the objectMapper
+        when(objectMapper.readValue(jsonStr, LinkedHashMap.class)).thenReturn((LinkedHashMap) data);
 
         // Call the genericMapData method
         List<Map<String, Object>> resultList = headerConfigurer.genericMapData(data, jsonStringKey);
@@ -49,7 +55,6 @@ public class HeaderConfigurerTest {
         assertTrue(value instanceof String);
 
         try {
-            
             // Assert that the deserialized map is equal to the original value
             assertEquals(data.get(jsonStringKey), resultList.get(0).get(jsonStringKey));
         } catch (Exception e) {
