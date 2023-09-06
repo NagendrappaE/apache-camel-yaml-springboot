4b60b0cae8249e9173c05f652ec6af0021e78079
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-09-06 23:56:39 +0530

    test

--- src/test/java/com/ece/camel/yaml/spring/HeaderConfigurerTest.java
+++ src/test/java/com/ece/camel/yaml/spring/HeaderConfigurerTest.java
@@ -1,10 +1,14 @@
 package com.ece.camel.yaml.spring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.util.LinkedHashMap;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,48 +23,40 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HeaderConfigurerTest {
-    
-    @Mock
-    private ObjectMapper objectMapper;
 
-    @InjectMocks
-    private HeaderConfigurer headerConfigurer;
+	@Mock
+	private ObjectMapper objectMapper;
 
-    public HeaderConfigurerTest() {
-        MockitoAnnotations.openMocks(this);
-    }
-    
-    @Test
-    public void testGenericMapData() throws JsonMappingException, JsonProcessingException {
-        // Create a sample data map
-        String jsonStr = "{\"ETL_ID\":\"John\",\"ETL_TS\":\"12-02-2023\",\"CONTENT\":{\"name\":\"abc\"}}";
-        Map<String, Object> data = new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+	@InjectMocks
+	private HeaderConfigurer headerConfigurer;
 
-        // Specify the jsonStringKey
-        String jsonStringKey = "CONTENT";
+	public HeaderConfigurerTest() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-        // Mock the behavior of the objectMapper
-        when(objectMapper.readValue(jsonStr, LinkedHashMap.class)).thenReturn((LinkedHashMap) data);
+	@Test
+	public void testGenericMapData() throws JsonMappingException, JsonProcessingException {
+		// Mock data
+        List<Object> linkedList = Arrays.asList("item1");
+        
+        Map<String, Object> data = new HashMap<>();
+        data.put("key1", "value1");
+        String jsonStringKey = "jsonKey";
 
-        // Call the genericMapData method
-        List<Map<String, Object>> resultList = headerConfigurer.genericMapData(data, jsonStringKey);
+       
 
-        // Verify the results
-        assertEquals(1, resultList.size());
+        // Call the method you want to test
+        List<Map<String, Object>> result = headerConfigurer.listOfMap(linkedList, data, jsonStringKey);
 
-        Map<String, Object> resultMap = resultList.get(0);
-        assertTrue(resultMap.containsKey(jsonStringKey));
+     
 
-        Object value = resultMap.get(jsonStringKey);
-        assertTrue(value instanceof String);
+        // Verify the result
+        assertEquals(linkedList.size(), result.size());
 
-        try {
-            // Assert that the deserialized map is equal to the original value
-            assertEquals(data.get(jsonStringKey), resultList.get(0).get(jsonStringKey));
-        } catch (Exception e) {
-            // If deserialization fails, fail the test
-            e.printStackTrace();
-            assertTrue(false);
+        // Verify that each map in the result contains the expected data
+        for (Map<String, Object> map : result) {
+            assertEquals("\"item1\"", map.get(jsonStringKey));
+            // You can add more assertions for other keys/values in the map if needed
         }
-    }
+	}
 }
